2e5630a812c6c6a880c69bc5713db589a917a228
diegocamy <53247032+diegocamy@users.noreply.github.com>
2020-01-03 21:50:14 -0300

    Updates

---
+++ precache-manifest.625c39effbf0fc03b17bb4542ffce7f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a717ac2a2d84019a3d9871dac8f65d6",
+    "url": "/diegocamy/rick-and-morty-infoapp/index.html"
+  },
+  {
+    "revision": "29bb373f8a9c1bfa7014",
+    "url": "/diegocamy/rick-and-morty-infoapp/static/js/2.644c3b1b.chunk.js"
+  },
+  {
+    "revision": "56396f11670a418ceb3e1565b1d5aac5",
+    "url": "/diegocamy/rick-and-morty-infoapp/static/js/2.644c3b1b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "59ce12f4d24cba5e721d",
+    "url": "/diegocamy/rick-and-morty-infoapp/static/js/main.612b0254.chunk.js"
+  },
+  {
+    "revision": "eccda09b026fd56ae83a",
+    "url": "/diegocamy/rick-and-morty-infoapp/static/js/runtime-main.00131b5b.js"
+  },
+  {
+    "revision": "75509f2ec9725b3cef07eefe651a4a8f",
+    "url": "/diegocamy/rick-and-morty-infoapp/static/media/logo.75509f2e.png"
+  }
+]);
